d25a65bfea2247d82384a52b8d681ec61b9c9756
Madeline Streilein <mcstreilein@gmail.com>
2019-10-27 14:28:55 -0400

    Restructure, style Landing page

--- server/client/single-purpose-app/src/components/Landing.js
+++ server/client/single-purpose-app/src/components/Landing.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 // import PropTypes from "prop-types";
 import ButtonComponent from './ButtonComponent'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.svg'
 
 export class Landing extends Component {
   render() {
 
     return (
-      <React.Fragment>
-        <h1>Fatcat Landing Page</h1>
-        <Link to="/login">
-          <ButtonComponent text={"Get Started"} classNames={'mdc-button--raised'} />
-        </Link>
-      </React.Fragment>
+      <div className="page landing-page">
+        <div className="page__content">
+          <img src={logo} alt="Logo" />
+          <p>
+            Track when you last completed an important task. Never forget when
+            you last fed the cat again.
+          </p>
+          <Link to="/login">
+            <ButtonComponent
+              text={"Get Started"}
+              classNames={"mdc-button--raised"}
+            />
+          </Link>
+        </div>
+      </div>
     );
   }
 }
