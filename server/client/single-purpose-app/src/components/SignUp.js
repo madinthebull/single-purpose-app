092f5d1e4e6a806c5f8dfd0582ef0026428114ab
Madeline Streilein <mcstreilein@gmail.com>
2019-10-27 14:27:03 -0400

    Add TextField, logo to SignUp page

--- server/client/single-purpose-app/src/components/SignUp.js
+++ server/client/single-purpose-app/src/components/SignUp.js
@@ -2,50 +2,81 @@ import React, { Component } from "react";
 // import PropTypes from "prop-types";
 import ButtonComponent from "./ButtonComponent";
 import { Link } from "react-router-dom";
+import logo from '../assets/logo.svg'
+
+// import Material Design components
+import TextField, { HelperText, Input } from "@material/react-text-field";
 
 export class SignUp extends Component {
-         state = {
-           buttons: [
-             {
-               text: "CANCEL",
-               id: 1,
-               outlined: true,
-               raised: true,
-               unelevated: false,
-               classNames: ""
-             },
-             {
-               text: "CREATE ACCOUNT",
-               id: 2,
-               outlined: false,
-               raised: true,
-               unelevated: false,
-               classNames: ""
-             }
-           ]
-         };
-         render() {
-           const { buttons } = this.state;
+  state = {
+    username: '',
+    password: '',
+    passwordConfirmation: '',
+    buttons: [
+      {
+        text: "CANCEL",
+        id: 1,
+        outlined: true,
+        raised: true,
+        unelevated: false,
+        classNames: ""
+      },
+      {
+        text: "CREATE ACCOUNT",
+        id: 2,
+        outlined: false,
+        raised: true,
+        unelevated: false,
+        classNames: ""
+      }
+    ]
+  };
+  render() {
+    const { buttons } = this.state;
 
-           return (
-             <React.Fragment>
-               <h1>FatCat Signup Page</h1>
-               <form>
-                 <input type="text" name="username"></input>
-                 <br />
-                 <input type="text" name="passwordCreation"></input>
-                 <br />
-                 <input type="text" name="passwordConfirmation"></input>
-                 <br />
-                 {buttons.map(button => (
-                   <Link to="/log">
-                     <ButtonComponent text={button.text} key={button.id} outlined={button.outlined} raised={button.raised} unelevated={button.unelevated} />
-                   </Link>
-                 ))}
-               </form>
-             </React.Fragment>
-           );
-         }
-       }
+    return (
+      <React.Fragment>
+        <img src={logo} alt="Logo" style={{ margin:'50px' }} />
+        <form>
+          <TextField
+            label="Username"
+            helperText={<HelperText>Enter a username.</HelperText>}
+          >
+            <Input value={this.state.username} onChange={this.onInputChange} />
+          </TextField>
+          <br />
+          <TextField
+            label="Create a password"
+            trailingIcon={<span className="material-icons">visibility</span>}
+            helperText={<HelperText>Enter a password.</HelperText>}
+          >
+            <Input value={this.state.passwordConfirmation} onChange={this.onInputChange} />
+          </TextField>
+          <br />
+          <TextField
+            label="Confirm password"
+            trailingIcon={<span className="material-icons">visibility</span>}
+            helperText={<HelperText>Enter your password again.</HelperText>}
+          >
+            <Input value={this.state.username} onChange={this.onInputChange} />
+          </TextField>
+          <br />
+
+          {buttons.map(button => (
+            <Link to="/log">
+              <ButtonComponent
+                text={button.text}
+                key={button.id}
+                outlined={button.outlined}
+                raised={button.raised}
+                unelevated={button.unelevated}
+              />
+            </Link>
+          ))}
+        </form>
+      </React.Fragment>
+    );
+  }
+}
 
 export default SignUp;
