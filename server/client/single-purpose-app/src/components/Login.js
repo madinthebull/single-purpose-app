58d441cb80e0c03f6441099db4fe30e28daa588e
Madeline Streilein <mcstreilein@gmail.com>
2019-10-27 14:28:03 -0400

    Add TextField, logo to SignUp page

--- server/client/single-purpose-app/src/components/Login.js
+++ server/client/single-purpose-app/src/components/Login.js
@@ -2,50 +2,52 @@ import React, { Component } from "react";
 // import PropTypes from "prop-types";
 import ButtonComponent from "./ButtonComponent";
 import { Link } from "react-router-dom";
+import logo from '../assets/logo.svg'
 
 export class Landing extends Component {
-         state = {
-           buttons: [
-             { text: "SIGN UP", id: 1, outlined: true, raised: true, unelevated: false, classNames: '', },
-             { text: "LOG IN", id: 2, outlined: false, raised: true, unelevated: false, classNames: '', }
-           ]
-         };
-         render() {
-           const { buttons } = this.state;
+  state = {
+    buttons: [
+      { text: "SIGN UP", id: 1, outlined: true, raised: true, unelevated: false, classNames: '', },
+      { text: "LOG IN", id: 2, outlined: false, raised: true, unelevated: false, classNames: '', }
+    ]
+  };
 
-           return (
-             <React.Fragment>
-               <h1>FatCat Login Page</h1>
-               <form>
-                 <input type="text" name="username"></input>
-                 <br />
-                 <input type="password" name="password"></input>
-                 <br />
+  render() {
+    const { buttons } = this.state;
 
-                 <Link to="/signup">
-                   <ButtonComponent
-                     text={buttons[0].text}
-                     key={buttons[0].id}
-                     raised={buttons[0].raised}
-                     unelevated={buttons[0].unelevated}
-                     outlined={buttons[0].outlined}
-                     classNames={buttons[0].className}
-                   />
-                 </Link>
-                 <Link to="/log">
-                   <ButtonComponent
-                     text={buttons[1].text}
-                     key={buttons[1].id}
-                     raised={buttons[0].raised}
-                     unelevated={buttons[0].unelevated}
-                     outlined={buttons[0].outlined}
-                     classNames={buttons[1].className}
-                   />
-                 </Link>
-               </form>
-             </React.Fragment>
-           );
-         }
-       }
+    return (
+      <React.Fragment>
+        <img src={logo} alt="Logo" />
+        <form>
+          <input type="text" name="username"></input>
+          <br />
+          <input type="password" name="password"></input>
+          <br />
+
+          <Link to="/signup">
+            <ButtonComponent
+              text={buttons[0].text}
+              key={buttons[0].id}
+              raised={buttons[0].raised}
+              unelevated={buttons[0].unelevated}
+              outlined={buttons[0].outlined}
+              classNames={buttons[0].className}
+            />
+          </Link>
+          <Link to="/log">
+            <ButtonComponent
+              text={buttons[1].text}
+              key={buttons[1].id}
+              raised={buttons[0].raised}
+              unelevated={buttons[0].unelevated}
+              outlined={buttons[0].outlined}
+              classNames={buttons[1].className}
+            />
+          </Link>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Landing;
