d3be30903bbf2271453de51ca13d4b1c6b739d84
Madeline Streilein <mcstreilein@gmail.com>
2019-10-27 21:35:27 -0400

    Fetch entries from db

--- server/client/single-purpose-app/src/App.js
+++ server/client/single-purpose-app/src/App.js
@@ -6,62 +6,29 @@ import Login from './components/Login'
 import SignUp from './components/SignUp'
 
 import axios from 'axios'
-import './App.scss'
+import './assets/sass/main.scss'
 
 class App extends Component {
   state = {
-    entries: [
-      // dummy data
-      {
-        id: 1,
-        amount: "1/2 cup",
-        date: "October 12, 2019 11:11AM",
-        user: 'Drew',
-      },
-      {
-        id: 2,
-        amount: "1/4 cup",
-        date: "October 12, 2019 05:00PM",
-        user: 'Drew',
-      },
-      {
-        id: 3,
-        amount: '1/8 cup',
-        date: 'October 13, 2019 8:11AM',
-        user: "Drew's girlfriend",
-      },
-      {
-        id: 4,
-        amount: '1/2 cup',
-        date: 'October 13, 2019 5:00PM',
-        user: 'Drew',
-      },
-      {
-        id: 5,
-        amount: '1/3 cup',
-        date: 'October 14, 2019 7:36AM',
-        user: 'Drew',
-      },
-    ]
+    entries: [],
   };
 
-  // componentDidMount() {
-  //   axios
-  //     .get("https://dev-fatcat.pantheonsite.io/measurements", {
-  //       headers: { "X-Requested-With": "XMLHttpRequest" }
-  //     })
-  //     .then(response =>
-  //       this.setState({
-  //         id: response.data.id,
-  //         measurement: response.data.measurement,
-  //         date: response.data.date
-  //       })
-  //     );
-  // }
-
+  componentDidMount() {
+    axios
+      .get("https://dev-fatcat.pantheonsite.io/measurements", {
+        headers: {
+          Accept: "application/hal+json",
+          Authorization: "Basic ZmF0Y2F0OmZhdGNhdA=="
+        }
+      })
+      .then(response =>
+        this.setState({
+          entries: response.data
+        })
+      );
+  }
 
   render() {
-    // pass dummy data to Entry Log component
     return (
       <BrowserRouter>
         <Switch>
--- server/client/single-purpose-app/src/components/Entry.js
+++ server/client/single-purpose-app/src/components/Entry.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
+import moment from 'moment';
 
 class Entry extends Component {
   render () {
     // destructure props
     const { entry } = this.props;
-
+    const date = this.props.entry;
+    const parsedDate = moment(date).format('L');
+    const parsedTime = moment(date).format('LT');
     return (
       <React.Fragment>
         <tr className="mdc-data-table__row">
-          <td className="mdc-data-table__cell">{entry.date}</td>
-          <td className="mdc-data-table__cell mdc-data-table__cell--numeric">{entry.time}</td>
+          <td className="mdc-data-table__cell">{parsedDate}</td>
           <td className="mdc-data-table__cell mdc-data-table__cell--numeric">
-            {entry.amount}
+          {parsedTime}</td>
+          <td className="mdc-data-table__cell mdc-data-table__cell--numeric">
+            {entry.measurement}
           </td>
-          <td className="mdc-data-table__cell">{entry.user}</td>
+          <td className="mdc-data-table__cell">{entry.uid}</td>
         </tr>
       </React.Fragment>
     );
